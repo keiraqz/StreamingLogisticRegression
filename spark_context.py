b5e2e3404494f92274a3facfdfad89c654883f63
syi115 <christopher.peck@capitalone.com>
2016-10-22 19:07:05 -0400

    nutz

---
+++ spark_context.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.ml.regression import LinearRegression
+from pyspark.streaming import StreamingContext
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+
+conf = SparkConf().setMaster("local[2]")
+sc = SparkContext.getOrCreate(conf=conf)
+# ssc = StreamingContext(sc, 1)
+# sqlContext = SQLContext(sc)
+
+# load rdd
+path ="file:///home/syi115/GitHub/de_talk/data.csv"
+rdd = sc.textFile(path).map(
+    lambda line: line.split(',')
+)
+
+# column names
+keys = rdd.first()
+DataRow = namedtuple("DataRow", keys)
+
+# get data
+data = rdd.filter(
+  lambda row: row[0] != keys[0]
+).map(
+    lambda row: [int(x) for x in row]
+)
+# .map(
+#   lambda row: DataRow(*[int(val) for val in row])
+#)
+
+# featurize
+from pyspark.mllib.regression import LabeledPoint
+def parsePoint(row):
+    if row[-1] == -1:
+        label = 0
+    elif row[-1] == 1:
+        label = 1
+    else:
+        raise ValueError(row[-1])
+    return LabeledPoint(label=label, features=row[:-1])
+
+data = data.map(parsePoint)
+
+
+# do a training/test split
+train_data, test_data = data.randomSplit((0.8, 0.2), seed=1800009193L)
+
+# fit the model
+model = LogisticRegressionWithLBFGS.train(train_data, regType="l1")
+
